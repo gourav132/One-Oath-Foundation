1f64fa7deead4d309bb80ba8b5a0ad34020fae8c
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-03-05 03:00:10 +0530

    Done with form validation

--- src/Components/Contact Form/Contact.jsx
+++ src/Components/Contact Form/Contact.jsx
@@ -1,35 +1,96 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 export default function Contact() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (fromFields) => {
+        console.log(fromFields);
+    }
+
   return (
     <div className='w-10/12 m-auto h-screen flex items-center justify-items-end'>
         <div className='w-full'>
-        <h1 className='text-4xl font-bold'>Let's Get in <span className='text-teal-400'>Touch</span> </h1>
+        <h1 className='text-4xl font-bold text-gray-800'>Let's Get in <span className='text-teal-600'>Touch</span> </h1>
         <p className='font-semibold mt-1 text-gray-500'>Any questions? We would be happy to help you</p>
 
-        <form action="" className='w-6/12 mt-10'>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-6/12 mt-10'>
 
-            <div class="grid gap-6 md:grid-cols-2 mb-6">
+            <div className="grid gap-6 md:grid-cols-2 mb-6">
+
                 <div>
-                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-700">Name</label>
-                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-400 block w-full p-2.5" placeholder="John Doe" />
+                    <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-700">Name</label>
+                    <input 
+                        {...register("FullName", {
+                            required: "Please provide us with your name",
+                            minLength: {
+                                value: 3,
+                                message: "Please enter a valid name"
+                            }
+                        })} 
+                        type="text" id="first_name" 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-600 block w-full p-2.5" 
+                        placeholder="John Doe" 
+                    />
+                    <p id="helper-text-explanation" className="mt-2 text-xs text-red-500">{errors.FullName?.message}</p>
                 </div>
+
                 <div>
-                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-700">Phone number</label>
-                    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-400 block w-full p-2.5" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                    <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-700">Phone number</label>
+                    <input 
+                        {...register("PhoneNo", { 
+                            required: "Please provide your phone no.",
+                            pattern: {
+                                value: /^\+?([0-9]{2})?\s?-?\d{10}$/,
+                                message: "Invalid phone number format."
+                            } 
+                        })} 
+                        type="tel" id="phone" 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-600 block w-full p-2.5" 
+                        placeholder="+xx xxxxxxxxxx" 
+                    />
+                    <p id="helper-text-explanation" className="mt-2 text-xs text-red-500">{errors.PhoneNo?.message}</p>
                 </div>
 
             </div>
-            <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-700">Email address</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-400 block w-full p-2.5" placeholder="john.doe@company.com" required />
-            </div> 
-            <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-700">How can we help you?</label>
-                <textarea type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-400 block w-full p-2.5" placeholder="Message us your query and we will contact you rigth away" required />
+
+            <div className="mb-6">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">Email address</label>
+                <input 
+                    {...register("Email", { 
+                        required: "Please provide your email address",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Please enter a valid email address"
+                          }
+                    })} 
+                    type="email" id="email" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-600 block w-full p-2.5"
+                    placeholder="john.doe@company.com"
+                />
+                <p id="helper-text-explanation" className="mt-2 text-xs text-red-500">{errors.Email?.message}</p>
+            </div>
+
+            <div className="mb-6">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">How can we help you?</label>
+                <textarea 
+                    {...register("Message", { 
+                        required: "Your query is important to us",
+                        maxLength: {
+                            value: 1000,
+                            message: "Please limit your message to 1000 characters"
+                        }
+                    })}
+                    rows={3}
+                    type="email" id="email" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-1 hover:ring-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-600 block w-full p-2.5" 
+                    placeholder="Message us your query and we will get back to you shortly"
+                />
+                <p id="helper-text-explanation" className="mt-2 text-xs text-red-500">{errors.Message?.message}</p>
             </div> 
 
-            <button type="submit" class="text-white bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+            <button type="submit" className="text-white bg-teal-600 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
         </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,31 +15,32 @@ import React from 'react'
 export default function Navbar() {
   return (
 
-<div className='absolute w-full'>
-<nav class="border-gray-200 bg-gray-50 w-10/12 m-auto">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-16">
-    <a href="/" class="flex items-center" >
-        <span class="self-center text-xl font-bold whitespace-nowrap text-teal-400">One Oath Foundation</span>
-    </a>
+    <div className='absolute w-full'>
+        <nav className="border-gray-200 bg-gray-50 w-10/12 m-auto">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-16">
+                
+                <a href="/" className="flex items-center" >
+                    <span className="self-center text-xl font-bold whitespace-nowrap text-teal-600">One Oath Foundation</span>
+                </a>
 
-    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
-        <li>
-          <a href="/" class="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 " aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/" class="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Gallery</a>
-        </li>
-        <li>
-          <a href="/" class="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Get Quote</a>
-        </li>
-        <li>
-          <a href="/" class="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Contact Us</a>
-        </li>
-      </ul>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+                    <li>
+                        <a href="/" className="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 " aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="/" className="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="/" className="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Get Quote</a>
+                    </li>
+                    <li>
+                        <a href="/" className="block py-2 px-3 md:p-0 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 ">Contact Us</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
     </div>
-  </div>
-</nav>
-</div>
   )
 }
